3b682b1c24c686816234ab6d36ab1b0b11ae3156
Michael Mottola <mottola64@gmail.com>
2019-03-21 23:31:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,14 @@
-array = ["fiddleheads", "okra", "kohlrabi"]
 def oxford_comma(array)
-if array.length == 2
-  return "#{array[0]} and #{array[1]}"
-elsif 2 < array.length
-  array[-1].insert(0, "and ")
-end
+if array.length == 1
+  array.join
+  elsif
+  array.length == 2
+  array[-1] = "and #{array[-1]}"
+  array.join (" ")
+else
+  array.length > 2
+  array[-1] = "and #{array[-1]}"
   array.join(", ")
 end
+end
+print oxford_comma(array)
